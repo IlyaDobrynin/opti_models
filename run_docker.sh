68fbb85a595bfee2c4ab703234913bede22bf4b2
Ilya Dobrynin <iliadobrynin@yandex.ru>
2021-06-15 20:55:38 +0300

    [add]: add docker sh

---
+++ run_docker.sh
@@ -0,0 +1,18 @@
+PROJECT_DIR=${1}
+VERSION=${2:-latest}
+STORAGE=${3:-/usr/local/opti_models}
+
+mkdir -p ${share}
+
+docker container run\
+    -it\
+    --rm\
+    --gpus all\
+    --ipc host\
+    --name ntech_train\
+    -p 6006:6006\
+    -p 8888:8888\
+    -v ${PROJECT_DIR}:/workspace\
+    -v ${STORAGE}:/usr/local/opti_models\
+    ntechtrain:${VERSION}\
+    bash
